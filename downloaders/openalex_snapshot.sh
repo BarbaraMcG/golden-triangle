2ed1034768c8c61eeb3e32e5dd06924f53d11f2c
Kaveh Aryan <57283761+kavaryan@users.noreply.github.com>
2024-10-23 13:33:11 +0100

    Add openalex_snapshot_filter.py

--- downloaders/openalex_snapshot.sh
+++ downloaders/openalex_snapshot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-aws s3 sync "s3://openalex" "../data/openalex-full-data" --exclude "*" --include "data/works/*" --no-sign-request
+# on create cloud:
+cd /scratch/prj/dh_golden_triangle/full_data
+aws s3 sync "s3://openalex" openalex-snapshot  --exclude "*" --include "data/works/*" --no-sign-request
+ln -s /scratch/prj/dh_golden_triangle/full_data/openalex-snapshot ~/golden-triangle/data/openalex-full-data
