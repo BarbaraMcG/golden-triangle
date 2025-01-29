b6bf36dcc3842e8e124b03c5000a8cc0d23b1489
Kaveh Aryan <57283761+kavaryan@users.noreply.github.com>
2025-01-29 13:30:38 +0000

    Bug fix and add abstract reinversion

--- hpc-jobs/parse_single_year/parse_single_year.py
+++ hpc-jobs/parse_single_year/parse_single_year.py
@@ -10,7 +10,19 @@ source_file = Path("/scratch_tmp/prj/dh_golden_triangle/results_2022.jsonl")
 output_file = Path("/scratch_tmp/prj/dh_golden_triangle/filtered_results_2022.jsonl")
 DATA_JOURNALS_FILE = Path(__file__).parent / Path('data_journals.txt')
 
-DATA_JOURNALS = [x.strip() for x in DATA_JOURNALS_FILE.read_text().splitlines()]
+DATA_JOURNALS = [x.strip().lower() for x in DATA_JOURNALS_FILE.read_text().splitlines()]
+
+def re_invert_abstract(inverted_index):
+    if inverted_index == None:
+        return ''
+    
+    max_index = max([index for indices in inverted_index.values() for index in indices])
+    text_list = [""] * (max_index + 1)
+    for word, indices in inverted_index.items():
+        for index in indices:
+            text_list[index] = word
+    text = " ".join(text_list)
+    return text
 
 def get_single_jsonl_output(work: dict):
     if (work['type'] not in ['article', 'book-chapter', 'preprint'] or
@@ -22,16 +34,20 @@ def get_single_jsonl_output(work: dict):
     
     work['gt_is_data_journal'] = (work['primary_location']['source']['display_name'].lower()
             in DATA_JOURNALS)
+
+    if 'abstract_inverted_index' in work and work['abstract_inverted_index'] is not None:
+        work['gt_abstract'] = re_invert_abstract(work['abstract_inverted_index'])
     return work
     
 
 def process_single_year_jsonl():
     with source_file.open('r') as inf, output_file.open('w+') as outf:
-        for line in inf:
+        for linei, line in enumerate(inf):
+            # if linei == 1000: break # smoke test
             try:        
                 work = get_single_jsonl_output(json.loads(line.strip()))
                 if work:
-                    outf.write(line)
+                    outf.write(json.dumps(work) + '\n')
             except Exception as e:
                 print(e)
                 continue
